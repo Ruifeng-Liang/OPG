01addbae4f792000eff473d36f8870cd4ebdb930
no-dream <lrf123@buaa.edu.cn>
2020-11-08 00:01:10 +0800

    提交5

--- OPG.java
+++ OPG.java
@@ -13,9 +13,10 @@ import java.util.Stack;
  * @Created by lrf
  */
 public class OPG {
-    static HashMap <Character,Integer>nt=new HashMap();
-    static int [][] priority={{2,1,1,2,1,1},{-1,1,-1,-1,1,1},{-1,1,1,-1,1,1},{-1,-1,-1,-1,0,2},{2,1,1,2,1,1},{-1,-1,-1,-1,2,0}};
-    static Stack<Character> stack=new Stack<>();
+    static HashMap<Character, Integer> nt = new HashMap();
+    static int[][] priority = {{2, 1, 1, 2, 1, 1}, {-1, 1, -1, -1, 1, 1}, {-1, 1, 1, -1, 1, 1}, {-1, -1, -1, -1, 0, 2}, {2, 1, 1, 2, 1, 1}, {-1, -1, -1, -1, 2, 0}};
+    static Stack<Character> stack = new Stack<>();
+
     public static String readbychar(String filename) {
         StringBuilder re = new StringBuilder();
         File file = new File(filename);
@@ -32,84 +33,81 @@ public class OPG {
         }
         return re.toString();
     }
+
     public static void main(String[] args) {
-       String msg=readbychar(args[0]);
-        System.out.println(msg);
-       StringBuilder stringBuilder=new StringBuilder(msg);
-       stringBuilder.append('#');
-       stack.push('#');
-       nt.put('i',0);
-       nt.put('+',1);
-       nt.put('*',2);
-       nt.put('(',3);
-       nt.put(')',4);
-       nt.put('#',5);
-       int j=0;
-       while(j<stringBuilder.length()){
-           char curchar=stringBuilder.charAt(j);
-           int topnt=stack.size()-1;
-           while(!nt.containsKey(stack.get(topnt))){
-               topnt--;
-           }
-           if(!nt.containsKey(curchar)){
-               System.out.println("E");
-               break;
-           }
-           int x1=nt.get(stack.get(topnt));
-           int y1=nt.get(curchar);
-           if(priority[x1][y1]==2){
-               System.out.println("E");
-               break;
-           }
-           if(priority[x1][y1]==-1){
-               System.out.println("I"+curchar);
-               stack.push(curchar);
-               j++;
-               if(stack.size()==2&&stack.peek()=='E'&&j==stringBuilder.length()-1){
-                   break;
-               }
-               continue;
-           }
-           if(priority[x1][y1]==1){
-               if(stack.get(topnt)=='i'){
-                   stack.pop();
-                   stack.push('E');
-                   System.out.println("R");
-                   if(stack.size()==2&&stack.peek()=='E'&&j==stringBuilder.length()-1){
-                       break;
-                   }
-                   continue;
-               }
-               if(stack.get(topnt)!='i'){
-                   if(topnt==stack.size()-2){
-                   stack.pop();
-                   stack.pop();
-                   stack.pop();
-                   stack.push('E');
-                   System.out.println("R");
-                   }else {
-                       System.out.println("RE");
-                       break;
-                   }
-               }
+        String msg = readbychar(args[0]);
+        StringBuilder stringBuilder = new StringBuilder(msg);
+        stringBuilder.append('#');
+        stack.push('#');
+        nt.put('i', 0);
+        nt.put('+', 1);
+        nt.put('*', 2);
+        nt.put('(', 3);
+        nt.put(')', 4);
+        nt.put('#', 5);
+        int j = 0;
+        while (j < stringBuilder.length()) {
+            char curchar = stringBuilder.charAt(j);
+            int topnt = stack.size() - 1;
+            while (!nt.containsKey(stack.get(topnt))) {
+                topnt--;
+            }
+            char topc=stack.get(topnt);
+            if (!nt.containsKey(curchar)) {
+                System.out.println("E");
+                break;
+            }
+            int x1 = nt.get(stack.get(topnt));
+            int y1 = nt.get(curchar);
+            if (priority[x1][y1] == 2) {
+                System.out.println("E");
+                break;
+            }
+            if (priority[x1][y1] == -1 || priority[x1][y1] == 0) {
+                System.out.println("I" + curchar);
+                stack.push(curchar);
+                j++;
+                if (stack.size() == 2 && stack.peek() == 'E' && j == stringBuilder.length() - 1) {
+                    break;
+                }
+                continue;
+            }
+            if (priority[x1][y1] == 1) {
+                if (stack.get(topnt) == 'i') {
+                    stack.pop();
+                    stack.push('E');
+                    System.out.println("R");
+                }
+                if (topc == ')') {
+                    if (stack.get(topnt - 1) == 'E' && stack.get(topnt - 2) == '(') {
+                        stack.pop();
+                        stack.pop();
+                        stack.pop();
+                        stack.push('E');
+                        System.out.println("R");
+                    } else {
+                        System.out.println("RE");
+                        break;
+                    }
+                }
+                if (topc=='+'||topc=='*') {
+                    if (topnt==stack.size()-2&&stack.get(topnt + 1) == 'E' && stack.get(topnt - 1) == 'E') {
+                        stack.pop();
+                        stack.pop();
+                        stack.pop();
+                        stack.push('E');
+                        System.out.println("R");
+                    } else {
+                        System.out.println("RE");
+                        break;
+                    }
+                }
+            }
+            if (stack.size() == 2 && stack.peek() == 'E' && j == stringBuilder.length() - 1) {
+                break;
+            }
 
-               }
-           if(priority[x1][y1]==0){
-               if(topnt==stack.size()-2) {
-                   stack.pop();
-                   stack.pop();
-                   stack.push('E');
-                   System.out.println("R");
-                   j++;
-               }else {
-                   System.out.println("RE");
-               }
-           }
-           if(stack.size()==2&&stack.peek()=='E'&&j==stringBuilder.length()-1){
-               break;
-           }
-
-       }
+        }
 
 
     }
